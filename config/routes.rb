193663ace6e626a0a9fe73cd9ccedf0926da8536
Lori M Olson <lori@wndx.com>
2011-09-16 16:31:32 -0600

    Basic configuration updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wimby2::Application.routes.draw do
+
   resources :wells
 
   # The priority is based upon order of creation:
@@ -50,7 +51,7 @@ Wimby2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'wells#map'
 
   # See how all your routes lay out with "rake routes"
 
